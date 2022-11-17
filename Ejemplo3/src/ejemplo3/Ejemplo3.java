0c2d9ae61c97dfbd155c5a696c0a30548ee95ec2
Zamora <Zamora@DESKTOP-998NS06>
2022-11-17 00:28:40 -0600

    Prueba num 3

---
+++ Ejemplo3/src/ejemplo3/Ejemplo3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo3;
+
+/**
+ *
+ * @author Zamora
+ */
+public class Ejemplo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Jazmin");
+        System.out.println("Sergio");
+    }
+    
+}
