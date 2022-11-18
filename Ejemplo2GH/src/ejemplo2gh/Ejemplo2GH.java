55ec74ca7e3ccc99ea16238c4e17de2139abf29a
Zamora <Zamora@DESKTOP-998NS06>
2022-11-17 23:55:38 -0600

    Prueba num 3.1

---
+++ Ejemplo2GH/src/ejemplo2gh/Ejemplo2GH.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo2gh;
+
+/**
+ *
+ * @author Sergio Aldebaran
+ */
+public class Ejemplo2GH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Sergio Tovar");
+         System.out.println("Jazmin Zamora");
+    }
+    
+}
